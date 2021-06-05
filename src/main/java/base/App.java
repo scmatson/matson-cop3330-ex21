0f52a6d44d5e9ae95c85706856ca9bc6a410558c
Scott <scmatson23@gmail.com>
2021-06-05 15:53:56 -0400

    Initial commit

---
+++ src/main/java/base/App.java
@@ -0,0 +1,30 @@
+package base;
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Scott Matson
+ */
+/*
+Exercise 21 - Numbers to Names
+
+Many programs display information to the end user in one form but use a different form inside the program.
+For example, you may show the word Blue on the screen, but behind the scenes you’ll have a numerical value for that color
+or an internal value because you may need to represent the textual description in another language for Spanish-speaking visitors.
+
+Write a program that converts a number from 1 to 12 to the corresponding month. Prompt for a number and display the corresponding
+calendar month, with 1 being January and 12 being December. For any value outside that range, display an appropriate error message.
+
+Example Output
+
+Please enter the number of the month: 3
+The name of the month is March.
+
+Constraints
+-Use a switch or case statement for this program.
+-Use a single output statement for this program.
+
+Challenges
+-Use a map or dictionary to remove the switch statement from the program.
+-Support multiple languages. Prompt for the language at the beginning of the program.
+ */
+public class App {
+}
