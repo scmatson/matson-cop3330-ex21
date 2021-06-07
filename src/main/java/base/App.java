56f48599243098f4f6f1f4dd55abed81ff0b67bb
Scott <scmatson23@gmail.com>
2021-06-06 20:46:08 -0400

    Exercise 21 Solution

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -1,4 +1,8 @@
 package base;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /*
  *  UCF COP3330 Summer 2021 Assignment 1 Solution
  *  Copyright 2021 Scott Matson
@@ -27,4 +31,76 @@ Challenges
 -Support multiple languages. Prompt for the language at the beginning of the program.
  */
 public class App {
+    static Scanner ui = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.print("Please enter the number of the month: ");
+
+        int month = range();
+        String name = Integer.toString(month);
+
+        switch(name)
+        {
+            case ("1"):
+                System.out.println("The name of the month is January.");
+                break;
+            case ("2"):
+                System.out.println("The name of the month is February.");
+                break;
+            case ("3"):
+                System.out.println("The name of the month is March.");
+                break;
+            case ("4"):
+                System.out.println("The name of the month is April.");
+                break;
+            case ("5"):
+                System.out.println("The name of the month is May.");
+                break;
+            case ("6"):
+                System.out.println("The name of the month is June.");
+                break;
+            case ("7"):
+                System.out.println("The name of the month is July.");
+                break;
+            case ("8"):
+                System.out.println("The name of the month is August.");
+                break;
+            case ("9"):
+                System.out.println("The name of the month is September.");
+                break;
+            case ("10"):
+                System.out.println("The name of the month is October.");
+                break;
+            case ("11"):
+                System.out.println("The name of the month is November.");
+                break;
+            case ("12"):
+                System.out.println("The name of the month is December.");
+                break;
+        }
+
+
+    }
+
+    public static int isNum() {
+        while (true) {
+            try {
+                return ui.nextInt();
+            } catch (InputMismatchException e) {
+                ui.next();
+                System.out.print("Sorry, please enter a number: ");
+            }
+        }
+    }
+
+    public static int range() {
+        while (true) {
+            int month = isNum();
+            if (month >= 1 && month <= 12) {
+                return month;
+            } else {
+                System.out.print("Enter a number between 1 and 12: ");
+            }
+        }
+    }
 }
